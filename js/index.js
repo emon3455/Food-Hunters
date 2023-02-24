9c75f342800a724c1e757a764d8d82f82e12b48b
emon3455 <emon.mhk69@gmail.com>
2023-02-25 00:45:06 +0600

    basic data load done

---
+++ js/index.js
@@ -0,0 +1,38 @@
+const loadData=(result)=>{
+    const url=`https://www.themealdb.com/api/json/v1/1/search.php?s=${result}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data=> displayData(data.meals))
+}
+
+const displayData=(data)=>{
+    
+    const container = document.getElementById("card-container");
+    container.innerHTML="";
+
+    data.forEach(element => {
+        console.log(element.strMeal);
+        const div = document.createElement("div");
+        div.classList.add("col");
+        div.innerHTML=`
+            <div class="card mb-2">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${element.strMealThumb}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">${element.strMeal}</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <a class="btn btn-warning fw-bold"> View Details </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        container.appendChild(div);
+    });
+
+}
+
+loadData("fish");
